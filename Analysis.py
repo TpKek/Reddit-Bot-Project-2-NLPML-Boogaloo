0d7ee5a0aac2b950952b6955f1caa9da54a0a0ab
TpK <Bertin.Dreyer1@gmail.com>
2021-10-20 21:40:40 +0200

    Goal semi-achieved

--- Analysis.py
+++ Analysis.py
@@ -1,55 +1,20 @@
 import praw
 from praw.models import MoreComments
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk.corpus import stopwords, twitter_samples
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
-import pandas as pd
 
 #use to filter out deduntant letters/words
 sw = set(stopwords.words("english"))
 punct = {".",",","!","?"}
 unwanted = set.union(sw,punct)
 
-#lexicon normalization (lemmatization reduces words to their base word, which is linguistically correct lemmas.)
-lem = WordNetLemmatizer()
-
-#tweet data to use for testing Reddit posts
-positive = twitter_samples.strings('positive_tweets.json')
-negative = twitter_samples.strings('negative_tweets.json')
-
-#create a dataframe from positive tweets
-df = pd.DataFrame(positive, columns=["Tweet"])
-
-#add a column to dataframe for positive sentiment value 1
-df["Sentiment"] = 1
-
-#create a dataframe for negative tweets
-df2 = pd.DataFrame(negative, columns=["Tweet"])
-
-#add a column to dataframe for negative sentiment value 0
-df2["Sentiment"] = 0
-
-#combine positive and negative tweets in one single dataframe
-df = df.append(df2, ignore_index=True)
-df = df.sample(frac = 1) 
-df.reset_index(drop=True, inplace=True)
-
-#displaying shape of dataset
-print("Dataset size:",df.shape)
-df.groupby("Sentiment").count()
-
-#converting all tweets to lowercase
-def convert_to_lowercase(tweet):
- return tweet.lower()
- 
-df["Tweet"] = df["Tweet"].apply(lambda x: convert_to_lowercase(x))
-
-#lexicon normalization (lemmatization reduces words to their base word, which is linguistically correct lemmas.)
-lem = WordNetLemmatizer()
-
 #create a sentiment instance for testing.
 sia = SentimentIntensityAnalyzer()
+#lexicon normalization 
+# #(lemmatization reduces words to their base word, which is linguistically correct lemmas.)
+lem = WordNetLemmatizer()
 
 #creating a reddit instance (if you want to read public posts, there is no need to login)
 reddit = praw.Reddit(
@@ -87,7 +52,19 @@ for comment in submission.comments:
 
     filtered_sentence = " ".join(filtered_list)
     #print(filtered_sentence)
+    
+    polscore = sia.polarity_scores(filtered_sentence)
 
+    #init var for later use
+    polRating = " "
+    if polscore["compound"] <= 0:
+        polRating = "Negative"
+    elif polscore["compound"] <= 0.5 and polscore["compound"] > 0:
+        polRating = "Neutral"
+    else :
+        polRating = "Positive"
+
+    #console test
     polscore = sia.polarity_scores(filtered_sentence)
     print("*******************************")
     print(comment.body)
@@ -96,4 +73,15 @@ for comment in submission.comments:
     print("sentence was rated as ", polscore['pos']*100, "% Positive")
     print("*******************************")
 
-    #TODO:Start with a training model to make sentiment more accurate
+    #using this to reply multiple lines in one comment/reply  
+    rep =""
+    rep += "sentence was rated as " + '%.2f' %(polscore['neg']*100) + "% Negative.\n\n"
+    rep += "sentence was rated as " + '%.2f' %(polscore['neu']*100) + "% Neutral.\n\n"
+    rep += "sentence was rated as " + '%.2f' %(polscore['pos']*100) + "% Positive.\n\n"
+    rep += "overall sentence rating conducted by VADER: "+ polRating + ".\n\n"
+
+    #post sentiment of comment as a reply
+    comment.reply(rep)
+
+    #NOTE: I Realised I tried doing what VADER already does.
+    #NOTE to self: Read the documentation properly next time, dummy.
