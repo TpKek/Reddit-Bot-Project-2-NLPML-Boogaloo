b4036c577a25624872c5098f8d10b93fbe012466
TpK <Bertin.Dreyer1@gmail.com>
2025-04-15 12:38:52 +0200

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -4,17 +4,35 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
+import string
+import time
 
 #use to filter out deduntant letters/words
 sw = set(stopwords.words("english"))
-punct = {".",",","!","?"}
-unwanted = set.union(sw,punct)
+punct = set(string.punctuation)
+unwanted = set.union(sw, punct)
 
 #create a sentiment instance for testing.
 sia = SentimentIntensityAnalyzer()
 #lexicon normalization 
 #(lemmatization reduces words to their base word, which is linguistically correct lemmas.)
 lem = WordNetLemmatizer()
+
+def process_comment(comment):
+    """
+    Process a Reddit comment by tokenizing, lemmatizing, and filtering unwanted words.
+
+    Args:
+        comment (str): The comment text.
+
+    Returns:
+        str: The processed comment.
+    """
+    comment_lower = comment.lower()
+    word_tokens = word_tokenize(comment_lower)
+    leml = [lem.lemmatize(word) for word in word_tokens]
+    filtered_list = [w for w in leml if not w in unwanted]
+    return " ".join(filtered_list)
 
 #creating a reddit instance (if you want to read public posts, there is no need to login)
 reddit = praw.Reddit(
@@ -25,7 +43,11 @@ reddit = praw.Reddit(
     password="")
 
 #grab a subreddit from reddit using reddit instance
-subreddit = reddit.subreddit("RBotProject")
+try:
+    subreddit = reddit.subreddit("RBotProject")
+except Exception as e:
+    print(f"Error accessing subreddit: {e}")
+    exit()
 
 #Check to see if it finds the designated subreddit post for the test
 for submission in subreddit.search("Testing Neural Network Sentiment Analysis"):
@@ -37,22 +59,11 @@ for submission in subreddit.search("Testing Neural Network Sentiment Analysis"):
 
 #read comments on subreddit
 for comment in submission.comments:
-    #making sure all the words in the comment is lowercase
-    comment_lower = comment.body.lower()
     #This is to ignore the "Load more comments" object on Reddit
     if isinstance(comment, MoreComments):
         continue
-    #tokenize comments (split a sentence into words or "tokens")
-    #used so I can eliminate more "neutral words" later on.
-    word_tokens = word_tokenize(comment_lower)
-    #lemmatize every word in word token list
-    leml = [lem.lemmatize(word) for word in word_tokens]
-    #print(leml)
-    filtered_list = [w for w in leml if not w in unwanted]
-    #print(filtered_list)
 
-    filtered_sentence = " ".join(filtered_list)
-    #print(filtered_sentence)
+    filtered_sentence = process_comment(comment.body)
     
     polscore = sia.polarity_scores(filtered_sentence)
 
@@ -60,11 +71,11 @@ for comment in submission.comments:
     #NOTE: I made an error here since 100% neutral comments come out as "Overall Negative", but I explained it in my README
     #won't change since it isn't nessasary.
     polRating = " "
-    if polscore["compound"] <= 0:
+    if polscore["compound"] < -0.05:
         polRating = "Negative"
-    elif polscore["compound"] <= 0.5 and polscore["compound"] > 0:
+    elif -0.05 <= polscore["compound"] <= 0.05:
         polRating = "Neutral"
-    else :
+    else:
         polRating = "Positive"
 
     #console test
@@ -86,6 +97,7 @@ for comment in submission.comments:
 
     #post sentiment of comment as a reply
     comment.reply(rep)
+    time.sleep(2)  # Wait 2 seconds between replies
 
     #NOTE: I realised I tried doing what VADER already does.
     #NOTE to self: Read the documentation properly next time, dummy.
